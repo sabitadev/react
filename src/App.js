b1651664d14560aaf65648887017a1ded50eff54
Sabita Samal <sabitasamal@Kobe.local>
2023-08-10 18:31:02 -0700

    commit test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Hello please reac me for react openings.
         </p>
         <a
           className="App-link"
